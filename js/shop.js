ef5bb638b1e290cca97114046eae695c3ebf46d1
dylan.robins <dylan.robins@gmail.com>
2017-07-03 11:23:10 -0400

    Changed Paypal button again

--- js/shop.js
+++ js/shop.js
@@ -28,10 +28,10 @@ $.get("json/shop.json", function(data) {
       <a href="#"><span class="card-title black-text">${price}€</span></a>
       <a href="#">
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="NLXPBZQUN5Z64">
-        <input type="image" src="http://thequarks.github.io/img/buynow.png" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+          <input type="hidden" name="cmd" value="_s-xclick">
+          <input type="hidden" name="hosted_button_id" value="XXUMTBP4ENKPC">
+          <input type="image" src="https://thequarks.github.io/img/buynow.png" border="0" name="submit" alt="Buy Now!">
+          <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
         </form>
       </a>
     </div>
