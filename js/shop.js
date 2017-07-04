12c18945dfbc22119e8fa366482e2a6df6b13192
dylan.robins <dylan.robins@gmail.com>
2017-07-04 06:20:16 -0400

    Change size of buy now button

--- js/shop.js
+++ js/shop.js
@@ -30,7 +30,7 @@ $.get("json/shop.json", function(data) {
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick">
           <input type="hidden" name="hosted_button_id" value="XXUMTBP4ENKPC">
-          <input type="image" src="https://thequarks.github.io/img/buynow.png" border="0" name="submit" alt="Buy Now!">
+          <input type="image" src="https://thequarks.github.io/img/buynow.png" width="110" border="0" name="submit" alt="Buy Now!">
           <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
         </form>
       </a>
